c277c6b8e8a673f7a2f8cc6a4542b38fae7b8de4
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-3.compute.internal>
2021-06-10 15:01:34 +0000

    E

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,15 +1,27 @@
 import json
 import boto3
+import uuid
 
 
 def lambda_handler(event, context):
     # TODO implement
-    dd = boto3.resource('dynamodb', region_name="eu-west-3")
 
-    t = dd.Table('crawler_processes')
+    dd = boto3.resource('dynamodb', region_name="eu-west-3")
+    crawler_threads_table = dd.Table('crawler_threads')
 
     for record in event['Records']:
-        if record['']
+        if record['eventName'] == "INSERT":
+            # Generate CrawlerThread
+
+            crawler_process = record['NewImage']
+
+            crawler_threads_table.put_item(
+                Item={
+                    'process_id': crawler_process['process_id'],
+                    'thread_id':   str(uuid.uuid4()),
+                    'crawler_engine': crawler_process['links']
+                }
+            )
 
     return {
         'statusCode': 200,
