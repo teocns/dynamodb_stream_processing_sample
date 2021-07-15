001d0fe2a9b4ec749ba3e72b6d67cb3f6789898e
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-3.compute.internal>
2021-07-15 18:13:57 +0000

    E

--- mysql_db/send_completed_process.py
+++ mysql_db/send_completed_process.py
@@ -8,15 +8,15 @@ import base64
 from botocore.exceptions import ClientError
 
 
-def do(crawler_process):
+def send(crawler_process):
     query = {
         'query': """call on_crawler_process_completed(%s,%s,%s,%s,%s,%s)""",
         'values': [
             crawler_process.get('url_id'),
-            crawler_process.get('scraped_jobs'),
-            crawler_process.get('scraped_links'),
+            crawler_process.get('jobs'),
+            crawler_process.get('links'),
             crawler_process.get('duplicates'),
-            crawler_process.get('bytes_transferred'),
+            crawler_process.get('bytes'),
             crawler_process.get('crawler_engine')
         ]
     }
--- pipelines.py
+++ pipelines.py
@@ -1,45 +1,53 @@
 from redis_cluster.functions import crawler_thread_in_queue
-from db_functions import ban_tracked_url, generate_main_thread_for_crawler_process
+from db_functions import generate_main_thread_for_crawler_process
+from mysql_db.send_completed_process import send as send_completed_process_to_mysql
 from config import MAX_CRAWLER_PROCESS_WITHOUT_JOBS
 from batch_write_items_model import BatchWriteItemsModel
 from typing import List
 
 
 def on_crawler_process_updated(cp, old_cp) -> List[BatchWriteItemsModel]:
-    total_threads = cp.get('crawler_threads_cnt')
-    done_threads = cp.get('crawler_threads_done_cnt')
+    done_threads = cp.get('threads_done_cnt')
 
-    if done_threads >= total_threads:
+    done_scraping_links = old_cp.get('done_scraping_links') == 1
+
+    links = cp.get('links')
+    duplicates = cp.get('duplicates')
+
+    real_threads_count = links - duplicates
+
+    if done_scraping_links and done_threads >= real_threads_count:
+        send_completed_process_to_mysql(cp)
         # Update status to completed
 
-        batch_write_items_models = {
-            'tracked_urls': BatchWriteItemsModel('tracked_urls')
-        }
+        # batch_write_items_models = {
+        #     'tracked_urls': BatchWriteItemsModel('tracked_urls')
+        # }
 
-        crawler_process_index = cp.get('crawler_process_index')
-        url_id = cp.get('url_id')
-        total_scraped_jobs = cp.get('total_scraped_jobs')
+        # crawler_process_index = cp.get('crawler_process_index')
+        # url_id = cp.get('url_id')
+        # total_scraped_jobs = cp.get('total_scraped_jobs')
 
-        tracked_urls_update = {
-            'crawler_engine': cp.get('crawler_engine'),
-            'total_scraped_jobs': total_scraped_jobs,
-            # 'crawler_processes_done_cnt': crawler_process_index + 1
-        }
+        # tracked_urls_update = {
+        #     'crawler_engine': cp.get('crawler_engine'),
+        #     'total_scraped_jobs': total_scraped_jobs,
+        #     # 'crawler_processes_done_cnt': crawler_process_index + 1
+        # }
 
-        if total_scraped_jobs == 0:
-            # 0 jobs scraped. Should change either crawler engine or ban URL
-            if crawler_process_index >= MAX_CRAWLER_PROCESS_WITHOUT_JOBS:
-                # If URL has been crawled more than threshold, it likely already tried another crawler engine so just proceed to ban the mf
-                # BAN URL
-                ban_tracked_url(url_id)
-            else:
-                tracked_urls_update['crawler_engine'] = 'SPIDER' if cp.get(
-                    'crawler_engine') == "SCRAPER" else 'SCRAPER'
-                # Retry with SPIDER
+        # if total_scraped_jobs == 0:
+        #     # 0 jobs scraped. Should change either crawler engine or ban URL
+        #     if crawler_process_index >= MAX_CRAWLER_PROCESS_WITHOUT_JOBS:
+        #         # If URL has been crawled more than threshold, it likely already tried another crawler engine so just proceed to ban the mf
+        #         # BAN URL
+        #         ban_tracked_url(url_id)
+        #     else:
+        #         tracked_urls_update['crawler_engine'] = 'SPIDER' if cp.get(
+        #             'crawler_engine') == "SCRAPER" else 'SCRAPER'
+        #         # Retry with SPIDER
 
-            # Get domains statistics for process
+        #     # Get domains statistics for process
 
-        return batch_write_items_models
+        # return batch_write_items_models
 
 
 def on_crawler_process_inserted(cp) -> List[BatchWriteItemsModel]:
