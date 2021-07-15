7f6d1998f163a29a17415bbc57e20a8512e641fa
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-3.compute.internal>
2021-07-15 19:21:10 +0000

    E

--- lambda_function.py
+++ lambda_function.py
@@ -27,24 +27,46 @@ def kill_redis_connection():
     redis_connection.close()
 
 
+def parse_image(image):
+    """Parse records incoming from Amazon DynamoDB Update"""
+
+    crawler_thread = {}
+    for key in image:
+        for value_type in image[key]:
+            raw_value = image[key][value_type]
+            value = None
+            if value_type == "S":
+                value = str(raw_value)
+            elif value_type == "N":
+                value = int(raw_value)
+            elif value_type == "BOOL":
+                value = bool(raw_value)
+
+            crawler_thread[key] = value
+
+    yield crawler_thread
+
+
 def lambda_handler(event, context):
     global redis_connection
 
     dd = boto3.resource('dynamodb', region_name="eu-west-3")
+
     crawler_threads_table = dd.Table('crawler_threads')
 
     for record in event['Records']:
+        crawler_process = parse_image(record['dynamodb']['NewImage'])
         if record['eventName'] == "INSERT":
             # Generate CrawlerThread
 
-            crawler_process = record['dynamodb']['NewImage']
+            crawler_process = parse_image(record['dynamodb']['NewImage'])
             # Generate crawler_thread
             on_crawler_process_inserted(crawler_process)
 
         if record['eventName'] == "UPDATE":
             on_crawler_process_updated(
-                record['dynamodb']['NewImage'],
-                record['dynamodb']['OldImage']
+                parse_image(record['dynamodb']['NewImage']),
+                parse_image(record['dynamodb']['OldImage'])
             )
 
     return {
