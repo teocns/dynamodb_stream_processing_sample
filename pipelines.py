a3774b967e7bbc568007c64a7702bf809e4f75af
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-3.compute.internal>
2021-09-03 16:27:48 +0000

    Release

--- pipelines.py
+++ pipelines.py
@@ -10,17 +10,17 @@ from typing import List
 def on_crawler_process_updated(cp, old_cp):
 
     done_threads = cp.get('threads_done_cnt',0) or 0
+    if old_cp.get('threads_done_cnt',0) or 0 > done_threads:
+        links = cp.get('links',0) or 0
+        duplicates = cp.get('duplicates',0) or 0
 
-    links = cp.get('links',0) or 0
-    duplicates = cp.get('duplicates',0) or 0
 
-
-    # Duplicates won't get scraped, and we add +1 for the scrape(LINKS) action
-    real_threads_count = links - duplicates + 1
-    
-    if done_threads >= real_threads_count:
-        update_tracked_url_after_completion(cp)
+        # Duplicates won't get scraped, and we add +1 for the scrape(LINKS) action
+        real_threads_count = links - duplicates + 1
         
+        if done_threads >= real_threads_count:
+            update_tracked_url_after_completion(cp)
+            
     
 
 def on_crawler_process_inserted(cp):
